38263558e1e30d5b4cc692694cca573b9e152e53
Chu-4hun <70685495+Chu-4hun@users.noreply.github.com>
2021-09-14 13:31:46 +0300

    Second try

---
+++ ASP.net_Web_Blog/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ASP.net_Web_Blog.Models;
+
+namespace ASP.net_Web_Blog.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ ASP.net_Web_Blog/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.net_Web_Blog.Models
+{
+    public class User
+    {
+        [Key]
+        
+        public int Id { get; set; }
+        
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
